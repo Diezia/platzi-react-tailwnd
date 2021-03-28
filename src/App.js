70c6337fd95da011729d1f3dcf8bd1fa41910acd
unknown <diezyael@gmail.com>
2021-03-28 15:41:32 -0300

    initialize project

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="bg-yellow-500">hello world</h1>
+      <input className="border-2 border-blue-500" type="text"/>
     </div>
   );
 }
